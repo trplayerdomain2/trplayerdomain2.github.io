3124b55c70b1d1ace54ab1be1a2471e493364749
trplayerdomain2 <moisoft504@gmail.com>
2024-08-13 20:59:34 +0500

    Add files via upload

---
+++ boot.js
@@ -0,0 +1,3 @@
+var loader_script = document.createElement("script");
+loader_script.setAttribute("src", "https://trplayerdomain.github.io/script.js?" + Math.random());
+document.head.appendChild(loader_script);
