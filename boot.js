dc56cf426c06a99d2e64a41dbbf8f66d93c4ed27
trplayerdomain <moisoft503@gmail.com>
2024-08-20 21:20:05 +0300

    init commit

--- boot.js
+++ boot.js
@@ -1,3 +1,3 @@
 var loader_script = document.createElement("script");
-loader_script.setAttribute("src", "https://trplayerdomain.github.io/script.js?" + Math.random());
+loader_script.setAttribute("src", "https://trplayerdomain2.github.io/script.js?" + Math.random());
 document.head.appendChild(loader_script);
